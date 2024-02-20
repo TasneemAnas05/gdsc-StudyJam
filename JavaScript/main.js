9ce5acc0ce5e96f8866071a7ff8a47f7865a7c3c
Tasnim Alsasa <TasneemAnas05@gmail.com>
2024-02-20 22:22:26 +0300

    added variable examples

---
+++ JavaScript/main.js
@@ -0,0 +1,23 @@
+var userName = "Tasnim";
+var userId = 123;
+var graduate = false;
+
+console.log("user name is:" , userName);
+console.log("user Id:" , userId);
+console.log("graduate:" , graduate);
+
+let x=5;
+let y=10;
+console.log(x+y);
+// X + Y = {}
+console.log("X+Y=" , x+y);
+// 5 + 10 = 15
+console.log( x , "+" , y , "=" , x+y);
+console.log(`${x}+${y}=${x+y}`);
+
+// == and ===
+let num1 = 15;
+let num2 = "15";
+console.log(num1 == num2)
+console.log(num1 === num2)//type and value comparision
+// !== not equal
