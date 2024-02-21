08ad5f023ec90e2dec9c93360eadb823a8f0f84f
Tasnim Alsasa <TasneemAnas05@gmail.com>
2024-02-21 21:10:29 +0300

    added for loop example

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -1,39 +1,56 @@
-var userName = "Tasnim";
-var userId = 123;
-var graduate = false;
+// var userName = "Tasnim";
+// var userId = 123;
+// var graduate = false;
 
-console.log("user name is:" , userName);
-console.log("user Id:" , userId);
-console.log("graduate:" , graduate);
+// console.log("user name is:" , userName);
+// console.log("user Id:" , userId);
+// console.log("graduate:" , graduate);
 
-let x=5;
-let y=10;
-console.log(x+y);
-// X + Y = {}
-console.log("X+Y=" , x+y);
-// 5 + 10 = 15
-console.log( x , "+" , y , "=" , x+y);
-console.log(`${x}+${y}=${x+y}`);
+// let x=5;
+// let y=10;
+// console.log(x+y);
+// // X + Y = {}
+// console.log("X+Y=" , x+y);
+// // 5 + 10 = 15
+// console.log( x , "+" , y , "=" , x+y);
+// console.log(`${x}+${y}=${x+y}`);
 
-// == and ===
-let num1 = 15;
-let num2 = "15";
-console.log(num1 == num2)
-console.log(num1 === num2)//type and value comparision
-// !== not equal
-graduate = true;
-if(graduate  ){
-    //action 
-    console.log(`${userName} is  graduated`)
-} 
-else{
-    console.log(`${userName} is not graduated`)
+// // == and ===
+// let num1 = 15;
+// let num2 = "15";
+// console.log(num1 == num2)
+// console.log(num1 === num2)//type and value comparision
+// // !== not equal
+// graduate = true;
+// if(graduate  ){
+//     //action 
+//     console.log(`${userName} is  graduated`)
+// } 
+// else{
+//     console.log(`${userName} is not graduated`)
+// }
+// if(userId === 123)
+//   console.log("Tasnim")
+// else if(userId === 124)
+//  console.log("Lujain")
+// else if (userId === 125)
+//  console.log("Batool")
+// else
+// console.log("Invalid Input")
+// //switch
+// let dayOfWeek = "Sunday"
+// switch (dayOfWeek){
+//     case "Sunday":
+//         console.log("it's Sunday");
+//         break;
+//     case "Friday":
+//         console.log("it's Friday");
+//         break;
+//     default :
+//     console.log("it's neither Sunday nor Friday ");
+    
+// }
+//print numbers from 0-5
+for(let i=0 ; i<=5 ; i++){
+    console.log(i);
 }
-if(userId === 123)
-  console.log("Tasnim")
-else if(userId === 124)
- console.log("Lujain")
-else if (userId === 125)
- console.log("Batool")
-else
-console.log("Invalid Input")
---
+++ JavaScript/popQuiz1.js
@@ -0,0 +1,9 @@
+let num1=10 , num2=20 , num3=5;
+if(num1 > num2 && num1 > num2)
+   console.log(num1);
+else if (num2 >num1 && num2 >num3)
+   console.log(num2)
+else
+  console.log(num3)
+
+cosole.log("using Math", Math.max(num1 , num2 , num3));
